16ef4df94f78bc0308d43560f4591289d9206c3d
Sri1263 <sriharikrishnan3621@gmail.com>
2026-01-11 08:12:43 +0530

    commit

--- two_sum/solution_2.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if not (2 <= len(nums) <= 10**4) or not (-10**9 <= target <= 10**9):
-            return 0
-        for i in range(len(nums)):
-            try:
-                return [i, nums.index(target-nums[i], i+1)]
-            except:
-                pass
--- two_sum/solution_3.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if not (2 <= len(nums) <= 10**4) or not (-10**9 <= target <= 10**9):
-            return 0
-        for i in range(len(nums)):
-            for j in range (i+1,(len(nums))):
-                if nums[i]+nums[j] == target:
-                    return [i, j]
